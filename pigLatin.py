956547f0c6e6abdd499106e53a52dfe36fabb385
stephzoo <stephanie.j.zhu@gmail.com>
2017-03-26 18:51:36 -0700

    Add files via upload

--- pigLatin.py
+++ pigLatin.py
@@ -4,35 +4,49 @@ import re
 # Translate a single word to Pig Latin
 def translate(word):
 	# Look out for special cases with starting letters
-	vowels = ['a', 'e', 'i', 'o', 'u']
+	vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 	cons_clust_duo = ['bl', 'br',  'ch', 'cl', 'cr', 'dr', 'fl', 'fr', 'gl', 'gr', 
 						'pl', 'pr', 'qu', 'sc', 'sh', 'sk', 'sl', 'sm', 'sn', 'sp', 
 						'st', 'sw', 'th', 'tr', 'tw', 'wh', 'wr']
 	cons_clust_trio = ['sch', 'scr', 'shr', 'sph', 'spl', 'spr', 'squ', 'str', 'thr']
 
 	# Actual translation of word to pig latin
-	if word[0] in vowels:
-		word = word + 'way'
-	elif triple_prefix(word) in cons_clust_trio:
-		word = word[3:] + triple_prefix(word) + "ay"
-	elif duo_prefix(word) in cons_clust_duo:
-		word = word[2:] + duo_prefix(word) + "ay"
-	else:
-		word = word[1:] + word[0] + "ay"
+	# If upper, then switch capitalized letter
+	if word[0].isupper():
+		if word[0] in vowels:
+			word = word + 'way'
+		elif triple_prefix(word) in cons_clust_trio:
+			word = word[3].upper() + word[4:] + triple_prefix(word).lower() + "ay"
 
+		elif duo_prefix(word) in cons_clust_duo:
+			word = word[2].upper() + word[3:] + duo_prefix(word).lower() + "ay"
+		else:
+			word = word[1].upper() + word[2:] + word[0].lower() + "ay"
+	else:
+		if word[0] in vowels:
+			word = word + 'way'
+		elif triple_prefix(word) in cons_clust_trio:
+			word = word[3:] + triple_prefix(word) + "ay"
+
+		elif duo_prefix(word) in cons_clust_duo:
+			word = word[2:] + duo_prefix(word) + "ay"
+		else:
+			word = word[1:] + word[0] + "ay"
 	return word
 	
 def duo_prefix(word):
 	if len(word) < 2:
 		return "no"
 	else:
-		return word[0] + word[1]
+		prefix = word[0] + word[1]
+		return prefix.lower()
 
 def triple_prefix(word):
 	if len(word) < 3:
 		return "no"
 	else:
-		return word[0] + word[1] + word[2]
+		prefix = word[0] + word[1] + word[2]
+		return prefix.lower()
 
 def main():
     phrase = str(raw_input("Input Sentence: \n"))
@@ -40,7 +54,7 @@ def main():
     sentence = p.findall(phrase)
 
     # Check for punctuation
-    punctuation = ['.', ',', '!', '?', ';']
+    punctuation = ['.', ',', '!', '?', ';', ':']
 
     # Translate each word in sentence, put back together
     new_sentence = ""
@@ -53,3 +67,4 @@ def main():
     print new_sentence
 
 main()
+
