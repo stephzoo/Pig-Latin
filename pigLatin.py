cdabd609d502af692d979edd824788881e4c64db
stephzoo <stephanie.j.zhu@gmail.com>
2017-03-26 18:30:10 -0700

    Add files via upload

---
+++ pigLatin.py
@@ -0,0 +1,55 @@
+## Pig latin script
+import re
+
+# Translate a single word to Pig Latin
+def translate(word):
+	# Look out for special cases with starting letters
+	vowels = ['a', 'e', 'i', 'o', 'u']
+	cons_clust_duo = ['bl', 'br',  'ch', 'cl', 'cr', 'dr', 'fl', 'fr', 'gl', 'gr', 
+						'pl', 'pr', 'qu', 'sc', 'sh', 'sk', 'sl', 'sm', 'sn', 'sp', 
+						'st', 'sw', 'th', 'tr', 'tw', 'wh', 'wr']
+	cons_clust_trio = ['sch', 'scr', 'shr', 'sph', 'spl', 'spr', 'squ', 'str', 'thr']
+
+	# Actual translation of word to pig latin
+	if word[0] in vowels:
+		word = word + 'way'
+	elif triple_prefix(word) in cons_clust_trio:
+		word = word[3:] + triple_prefix(word) + "ay"
+	elif duo_prefix(word) in cons_clust_duo:
+		word = word[2:] + duo_prefix(word) + "ay"
+	else:
+		word = word[1:] + word[0] + "ay"
+
+	return word
+	
+def duo_prefix(word):
+	if len(word) < 2:
+		return "no"
+	else:
+		return word[0] + word[1]
+
+def triple_prefix(word):
+	if len(word) < 3:
+		return "no"
+	else:
+		return word[0] + word[1] + word[2]
+
+def main():
+    phrase = str(raw_input("Input Sentence: \n"))
+    p = re.compile(r'\w+|[^\w\s]')
+    sentence = p.findall(phrase)
+
+    # Check for punctuation
+    punctuation = ['.', ',', '!', '?', ';']
+
+    # Translate each word in sentence, put back together
+    new_sentence = ""
+    for word in sentence:
+    	if word in punctuation:
+    		new_sentence = new_sentence[:-1] + word + " "
+    	else:
+    		word = translate(word)
+    		new_sentence = new_sentence + word + " "
+    print new_sentence
+
+main()
